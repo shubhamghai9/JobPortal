3577302fd79f2292ae552ac0832ca314340d5bc4
shubhamghai9 <shubhamghai9@gmail.com>
2025-08-12 01:06:04 +0530

    Add Vite React frontend

---
+++ jobportal-client/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:5001', // your .NET API URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
